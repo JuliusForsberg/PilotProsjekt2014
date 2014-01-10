f1b547d80aa8d181dcf06be684717363745612f9
martinlilleslaatten <nitram1407@gmail.com>
2014-01-10 10:42:02 +0100

    Startet with the enemy, and melee trap.

---
+++ Unity_Pilot/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float health = 20f;	
+
+	void Start(){
+	
+	}
+
+	void Update(){
+	
+	}
+
+	public void TakeDamage(float damage){
+		health -= damage;
+		Debug.Log("TookDamage");
+
+		if(health <= 0){
+			Debug.Log("DIED");
+	
+			StartCoroutine(WaitAndDie());
+			//Destroy (gameObject);
+		}
+	}
+
+	public bool isDead(){
+		if(health <= 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	private IEnumerator WaitAndDie(){
+		yield return new WaitForSeconds(0.2f);
+		Destroy(gameObject);
+	}
+}
---
+++ Unity_Pilot/Assets/Scripts/TrapMelee.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapMelee : MonoBehaviour {
+
+	public float cooldownTimer = 1f;
+	private float timer;
+
+	public float damage = 20f;
+	public int uses = 1;
+
+	private bool cooldown;
+	private bool triggered;
+
+	private ArrayList enemyList = new ArrayList();
+
+	void Start(){
+		timer = 0;
+		cooldown = false;
+		triggered = false;
+	}
+
+	void Update(){
+		if(cooldown){
+			timer += Time.deltaTime;
+
+			if(timer > cooldownTimer){
+				timer = 0;
+				cooldown = false;
+			}
+		}
+
+		if(triggered && !cooldown){
+			uses--;
+
+			if(enemyList.Count > 0){
+				for(int i=0; i<enemyList.Count; i++){
+					GameObject enemy = (GameObject)enemyList[i];
+					enemy.GetComponent<Enemy>().TakeDamage(damage);
+
+					if(enemy.GetComponent<Enemy>().isDead()){
+						enemyList.Remove(enemy);
+						Debug.Log ("Remove");
+					}
+				}
+			}
+
+			cooldown = true;
+			Debug.Log ("TrapTriggered");
+
+			if(enemyList.Count == 0){
+				triggered = false;
+			}
+			if(uses <= 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag=="Enemy"){
+			Debug.Log("Enter: " + col.gameObject);
+			enemyList.Add(col.gameObject);
+			triggered = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag=="Enemy"){
+			Debug.Log("Exit: " + col.gameObject);
+			enemyList.Remove(col.gameObject);
+		}
+	}
+}

4bd7ef5cc9ac4aa20e93bde9e2efe53a885a7895
JuliusForsberg <Juliuslf@hotmail.com>
2014-01-14 15:37:03 +0100

    You can chang the size of the highlighter (e.g. selected squares)
    Does not detect squares that are occupied when the size is more than one
    square.

--- Unity_Pilot/Assets/Scripts/TrapPlace.cs
+++ Unity_Pilot/Assets/Scripts/TrapPlace.cs
@@ -28,6 +28,9 @@ public class TrapPlace : MonoBehaviour {
 	public float gridSizeX=1;
 	public float gridSizeZ=1;
 	GameObject highLightObject;
+    int highlightSizeX=1;
+    int highlightSizeZ=1;
+    public GameObject createObject;
 
 	bool[,] gridSquares;
 	int coordX;
@@ -35,70 +38,128 @@ public class TrapPlace : MonoBehaviour {
 	//GameObject ball;
 
 	void Update () {
-	if(_enabled)
-	{
-		Ray ray = new Ray();
-		ray = camTopDown.camera.ScreenPointToRay(Input.mousePosition);
-		RaycastHit[] rayHits = Physics.RaycastAll(ray, 100);
+        if (_enabled)
+        {
+            Ray ray = new Ray();
+            ray = camTopDown.camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] rayHits = Physics.RaycastAll(ray, 100);
 
-		for(int i=0;i<rayHits.Length;i++)
-		{
-			if(rayHits[i].collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
-			{
-				hitObjects[i] = rayHits[i].collider.gameObject;
-				if(highLightObject == null)
-				{
-					highLightObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					highLightObject.transform.localScale = new Vector3(gridSizeX, 1, gridSizeZ);
-				}
+            for (int i = 0; i < rayHits.Length; i++)
+            {
+                if (rayHits[i].collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
+                {
+                    hitObjects[i] = rayHits[i].collider.gameObject;
+                    if (highLightObject == null)
+                    {
+                        highLightObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        highLightObject.transform.localScale = new Vector3(gridSizeX * highlightSizeX, 1, gridSizeZ * highlightSizeZ);
+                    }
 
-//				if(ball == null)
-//				{
-//					ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//					ball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-//					//ball.renderer.material.color = new Vector4(1.0f, 1.0f, 1.0f, 0.1f);
-//					ball.collider.enabled = false;
-//				}
-//				ball.transform.position = new Vector3(rayHits[i].point.x, rayHits[i].point.y, rayHits[i].point.z);
+                    //				if(ball == null)
+                    //				{
+                    //					ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    //					ball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    //					//ball.renderer.material.color = new Vector4(1.0f, 1.0f, 1.0f, 0.1f);
+                    //					ball.collider.enabled = false;
+                    //				}
+                    //				ball.transform.position = new Vector3(rayHits[i].point.x, rayHits[i].point.y, rayHits[i].point.z);
 
-				float offsetX = gridSizeX/2; //Offsets the grid squares so that the corners are in 0,0,0. Seems like a good idea at this time..
-				float offsetZ = gridSizeZ/2;
+                    float offsetX;
+                    float offsetZ;
 
-				float x = rayHits[i].point.x-offsetX;
-				float z = rayHits[i].point.z-offsetZ;
+                    if (highlightSizeX%2 == 1) //If the size is an odd number.
+                    {
+                    
+                        offsetX = gridSizeX / 2; //Offsets the grid squares so that the corners are in 0,0,0. Seems like a good idea at this time.. 
+                    }
+                    else
+                    {
+                        offsetX = 0; //Offset is not needed if the highlightobject is bigger than 1 square.
+                    }
+                    
+                    if (highlightSizeZ%2 == 1)
+                    {
+                    
+                        offsetZ = gridSizeZ / 2;
+                    }
+                    else
+                    {
+                        offsetZ = 0;
+                    }
 
-				x = gridSizeX*(Mathf.Round(x/gridSizeX));
-				z = gridSizeZ*(Mathf.Round(z/gridSizeZ));
+                    float x = rayHits[i].point.x - offsetX;
+                    float z = rayHits[i].point.z - offsetZ;
+
+                    x = (gridSizeX) * (Mathf.Round(x / gridSizeX ));
+                    z = (gridSizeZ) * (Mathf.Round(z / gridSizeZ ));
 
 
-				//print (gridSquares.Length);
-				
-				coordX = (int)( (x/gridSizeX)+4);
-				coordZ = (int)( (z/gridSizeZ)+4);
-				
-					Debug.Log((coordZ)+" "+(coordX)+(gridSquares[coordX, coordZ]));
+                    coordX = (int)((x / gridSizeX) + 4);
+                    coordZ = (int)((z / gridSizeZ) + 4);
 
-				highLightObject.transform.position = new Vector3(x+offsetX, rayHits[i].point.y, z+offsetZ);
-					if(gridSquares[coordX, coordZ] == true)
-					{
-						highLightObject.renderer.material.color = Color.red;
-					}
+                    Debug.Log((coordZ) + " " + (coordX) + (gridSquares[coordX, coordZ]));
 
+                    highLightObject.transform.position = new Vector3(x + offsetX, rayHits[i].point.y, z + offsetZ);
 
-				break;
+                    if (highlightSizeX > 1 || highlightSizeZ > 1)
+                    {
+                        for(int k=0;k<highlightSizeX;k++)
+                        {
+                            for(int j=0;j<highlightSizeZ;j++)
+                            {
+                                if( gridSquares[coordX + k, coordZ + j] == true)
+                                {
+                                    highLightObject.renderer.material.color = Color.red;
+                                    break;
+                                }
+                                highLightObject.renderer.material.color = Color.white;
+                            }
+                        }
 
-			}
-		}
-		if(rayHits.Length == 0)
-		{
-			Destroy(highLightObject);
-		}
+                    }
+                    else
+                    {
+                        if (gridSquares[coordX, coordZ] == true)
+                        {
+                            highLightObject.renderer.material.color = Color.red;
+                        }
+                        else
+                            highLightObject.renderer.material.color = Color.white;
+                    }
 
-		if(Input.GetKeyDown(KeyCode.R))
-		{
-			placeObject(GameObject.CreatePrimitive(PrimitiveType.Cube));
-		}
-	}
+                    break;
+
+                }
+                else if(i == rayHits.Length-1)
+                    Destroy(highLightObject);
+            }
+            if (rayHits.Length == 0)
+            {
+                Destroy(highLightObject);
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                placeObject(createObject);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                highlightSizeX = 1;
+                highlightSizeZ = 1;
+                Destroy(highLightObject);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                highlightSizeX = 3;
+                highlightSizeZ = 1;
+                Destroy(highLightObject);
+            }
+        }
+        else
+            Destroy(highLightObject);
+
 	}
 
 	void placeObject(GameObject _object) {
@@ -109,13 +170,10 @@ public class TrapPlace : MonoBehaviour {
 
 				if(gridSquares[coordX, coordZ] == false)
 					{
-						_object.SetActive(true);
-						_object.transform.position = pos;
+                        Instantiate(_object, pos, _object.transform.rotation);
 						gridSquares[coordX, coordZ] = true;
 					}
 			}
-			else
-				Destroy(_object);
 	}
 
 	void OnGUI() {
@@ -137,6 +195,6 @@ public class TrapPlace : MonoBehaviour {
 	void endConstruction () {
 		camMain.enabled = true;
 		camTopDown.enabled = false;
-		enabled = false;
+		_enabled = false;
 	}
 }

a73a7147b7a18eeedb6c5399ded333d7361edf5b
martinlilleslaatten <nitram1407@gmail.com>
2014-01-17 18:31:11 +0100

    Remade the WaveManager.
    Easier to set up in the editor. Added a wait feature.

--- Unity_Pilot/Assets/Scripts/WaveManager.cs
+++ Unity_Pilot/Assets/Scripts/WaveManager.cs
@@ -4,22 +4,16 @@ using System.Collections.Generic;
 
 public class WaveManager : MonoBehaviour {
 
-	public string[] waves;
+	public List<Wave> waves;
 	public GameObject[] enemyType;
-	public float spawnInterval = 1f;
-	public int waveNumber = -1;
+	public Vector2 spawnInterval;
+	public Vector2 positionOffsetXZ;
+	public int waveNumber = 0;
 
-	public int spawnerFinished;
+	private int[] spawnerFinished;
 
 	private Transform[] spawns;
 	private float[] nextSpawnTime;
-
-	private int[,] spawnNumber;
-
-	private int[] iteraterNumber;
-	private int[] iterator;
-
-	private List<int>[,] spawnArrayList;
 
 	//TESTING
 	private float nextActiveTime;
@@ -27,77 +21,17 @@ public class WaveManager : MonoBehaviour {
 	//------
 
 	void Start(){
-		spawnerFinished = 0;
+		foreach(Wave wave in waves){
+			wave.Initialize();
+		}
+
+		spawnerFinished = new int[]{0,0,0};
 
 		spawns = new Transform[transform.childCount];
 		spawns[0] = transform.FindChild("SpawnPointSouth");
 		spawns[1] = transform.FindChild("SpawnPointWest");
 		spawns[2] = transform.FindChild("SpawnPointEast");
-
-		spawnNumber = new int[transform.childCount,waves.Length];
-
-		spawnArrayList = new List<int>[3,2];
-		for(int e=0; e<3; e++){
-			List<int> spawnEnemyNumber = new List<int>();
-			List<int> spawnEnemyType = new List<int>();
-			spawnArrayList[e,0] = spawnEnemyNumber;
-			spawnArrayList[e,1] = spawnEnemyType;
-		}
-
-		for(int j=0; j<waves.Length; j++){
-			string[] splitWave;
-			splitWave = waves[j].Split(new char[] {'/'});
-
-			for(int k=0; k<splitWave.Length; k++){
-				if(splitWave[k] != "0"){
-					if(splitWave[k].Contains(",")){
-						string[] splitSpawn;
-						splitSpawn = splitWave[k].Split(new char[] {','});
-
-						for(int l=0; l<splitSpawn.Length; l++){
-							if(splitSpawn[l].Contains("-")){
-								string[] splitType;
-								splitType = splitSpawn[l].Split(new char[] {'-'});
-
-								spawnArrayList[k,0].Add(int.Parse(splitType[0]));
-								spawnArrayList[k,1].Add(int.Parse(splitType[1]));
-
-								spawnNumber[k,j] = (int)spawnNumber[k,j] + (int)spawnArrayList[k,0][spawnArrayList[k,0].Count-1];
-								//Debug.Log(splitType[0]);
-								//Debug.Log(splitType[1]);
-							}else{
-								Debug.Log("Specify type of enemy: 2");
-							}
-						}
-					}else{
-						if(splitWave[k].Contains("-")){
-							string[] splitType;
-							splitType = splitWave[k].Split(new char[] {'-'});
-
-							spawnArrayList[k,0].Add(int.Parse(splitType[0]));
-							spawnArrayList[k,1].Add(int.Parse(splitType[1]));
-
-							spawnNumber[k,j] = (int)spawnNumber[k,j] + (int)spawnArrayList[k,0][spawnArrayList[k,0].Count-1];
-							//Debug.Log(splitType[0]);
-							//Debug.Log(splitType[1]);
-						}else{
-							Debug.Log("Specify type of enemy: 1");
-						}
-					}
-				}else{
-					Debug.Log("Do nothing");
-				}
-			}
-		}
-		iteraterNumber = new int[transform.childCount];
-		iterator = new int[transform.childCount];
-
-		for(int sp=0; sp<iteraterNumber.Length; sp++){
-			for(int list=0; list<spawnArrayList[sp, 0].Count; list++){
-				iteraterNumber[sp] += spawnArrayList[sp,0][list];
-			}
-		}
-
+		
 		nextSpawnTime = new float[transform.childCount];
 	}
 
@@ -108,21 +42,22 @@ public class WaveManager : MonoBehaviour {
 
 	void Update(){
 		if(waveActive){
-			if(waveNumber < waves.Length){
+			if(waves.Count > 0){
 				for(int i=0; i<spawns.Length; i++){
-					if(spawnNumber[i,waveNumber] > 0){
-						if(spawnArrayList[i,0][iterator[i]] > 0){
-							if(Time.time >= nextSpawnTime[i]){
-								SpawnEnemy(i, spawnArrayList[i,1][iterator[i]]);
-								spawnArrayList[i,0][iterator[i]]--;
+					if(Time.time >= nextSpawnTime[i]){
+						if(waves[0].GetSpawn(i).SpawnNumber > 0){
+							int enemyType = waves[0].GetSpawn(i).EnemyType;
+
+							//Spawn as normal. Else, go into sleep mode. Set type to -2 to prevent multiple Coroutines.
+							if(enemyType >= 0){
+								SpawnEnemy(i, enemyType);
+							}else if(enemyType == -1){
+								waves[0].GetSpawn(i).EnemyType = -2;
+								StartCoroutine(SleepForSeconds((float)waves[0].GetSpawn(i).GetEnemyCount(), i));
 							}
-						}else if(iterator[i] < iteraterNumber[i]){
-							iterator[i]++;
-							//Debug.Log("It++: " + iterator[i] + "Spawn: " + i);
+						}else if(spawnerFinished[i] != 1){
+							spawnerFinished[i] = 1;
 						}
-					}else if(spawnNumber[i,waveNumber] != -1){
-						spawnerFinished++;
-						spawnNumber[i,waveNumber] = -1;
 					}
 				}
 			}else{
@@ -130,12 +65,23 @@ public class WaveManager : MonoBehaviour {
 			}
 		}
 
-		if(spawnerFinished == 3){
-			spawnerFinished = 0;
-			waveActive = false;
-			//gameObject.SetActive(false);
+		if(spawnerFinished[0] == 1){
+			if(spawnerFinished[1] == 1){
+				if(spawnerFinished[2] == 1){
+					spawnerFinished[0] = 0;
+					spawnerFinished[1] = 0;
+					spawnerFinished[2] = 0;
 
-			nextActiveTime = Time.time + 3f;
+					waves.RemoveAt(0);
+
+					//TESTING
+					waveActive = false;
+					//gameObject.SetActive(false);
+					//------
+
+					nextActiveTime = Time.time + 3f;
+				}
+			}
 		}
 
 		//TESTING
@@ -145,10 +91,111 @@ public class WaveManager : MonoBehaviour {
 		//------
 	}
 
-	private void SpawnEnemy(int num, int spawnType){
-		spawnNumber[num,waveNumber]--;
-		nextSpawnTime[num] = Time.time + spawnInterval;
+	private void SpawnEnemy(int num, int spawnType=0){
+		waves[0].GetSpawn(num).SpawnNumber = 1;
+		nextSpawnTime[num] = Time.time + Random.Range(spawnInterval.x, spawnInterval.y);
 
-		Instantiate(enemyType[spawnType].gameObject, spawns[num].position, spawns[num].rotation);
+		Vector3 spawnPoint = new Vector3(spawns[num].position.x, spawns[num].position.y, spawns[num].position.z);
+		spawnPoint.x = Random.Range(spawnPoint.x - positionOffsetXZ.x, spawnPoint.x + positionOffsetXZ.x);
+		spawnPoint.z = Random.Range(spawnPoint.z - positionOffsetXZ.y, spawnPoint.z + positionOffsetXZ.y);
+		Instantiate(enemyType[spawnType].gameObject, spawnPoint, spawns[num].rotation);
+	}
+
+	private IEnumerator SleepForSeconds(float seconds, int spawnNumber){
+		//Debug.Log("Start: " + Time.time);
+		yield return new WaitForSeconds(seconds);
+		//Debug.Log("End: " + Time.time);
+		waves[0].GetSpawn(spawnNumber).RemoveInternalWave();
+	}
+}
+
+[System.Serializable]
+public class Wave{
+	public Spawn southSpawn;
+	public Spawn westSpawn;
+	public Spawn eastSpawn;
+
+	public void Initialize(){
+		southSpawn.Initialize();
+		westSpawn.Initialize();
+		eastSpawn.Initialize();
+	}
+
+	public Spawn GetSpawn(int spawnNumber){
+		switch(spawnNumber){
+		case 0:
+			return southSpawn;
+		case 1:
+			return westSpawn;
+		case 2:
+			return eastSpawn;
+		default:
+			return southSpawn;
+		}
+	}
+}
+
+[System.Serializable]
+public class Spawn{
+	public List<string> internalWaves;
+	
+	private List<int> enemyCount;
+	private List<int> enemyType;
+
+	public void Initialize(){
+		enemyCount = new List<int>();
+		enemyType = new List<int>();
+
+		for(int i=0; i<internalWaves.Count; i++){
+			if(internalWaves[i].Contains("#")){
+				//Remove the "#".
+				internalWaves[i] = internalWaves[i].Remove(0,1);
+
+				//Add the wait amount to enemyCount, set the type to -1, to check later.
+				enemyCount.Add(int.Parse(internalWaves[i]));
+				enemyType.Add(-1);
+			}else{
+				//Split the two numbers, add them to their list.
+				string[] split = internalWaves[i].Split(new char[]{'-'});
+
+				enemyCount.Add(int.Parse(split[0]));
+				enemyType.Add(int.Parse(split[1]));
+			}
+		}
+	}
+
+	public int EnemyType{
+		get{
+			return enemyType[0];
+		}
+		set{
+			enemyType[0] = value;
+		}
+	}
+
+	public int GetEnemyCount(){
+		return enemyCount[0];
+	}
+
+	public void RemoveInternalWave(){
+		//Debug.Log ("Remove");
+		enemyCount.RemoveAt(0);
+		enemyType.RemoveAt(0);
+		internalWaves.RemoveAt(0);
+	}
+	
+	public int SpawnNumber{
+		get{
+			//Debug.Log ("Return: " + enemyCount.Count);
+			return internalWaves.Count;
+		}
+		set{
+			enemyCount[0] -= value;
+			//Debug.Log("Set: " + enemyCount[0]);
+
+			if(enemyCount[0] <= 0){
+				RemoveInternalWave();
+			}
+		}
 	}
 }
